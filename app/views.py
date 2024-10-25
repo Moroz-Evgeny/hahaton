25029bba96268d7d4cbb83a5e39a5844a94ac6d0
Moroz-Evgeny <ya.moroz2020@gmail.com>
2024-10-25 22:03:00 +0300

    regiser and login

--- app/views.py
+++ app/views.py
@@ -1,14 +1,29 @@
-from flask import Blueprint, render_template, session, flash, redirect, url_for
-
+from flask import Blueprint, render_template, request, session, flash, redirect, url_for
+from app.models import User
+from app import db
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def index():
-    try:
-        return render_template('register.html')
-    except KeyError:
-        return render_template('index.html')
+@main.route('/', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+        login = request.form['log']
+        email = request.form['email_']
+        password = request.form['pass']
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Пользователь с таким email уже зарегистрован')
+        new_user = User(login=login, email=email, password=password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        session['userReg'] = True
+        return redirect(url_for('main.register'))
+    
+    return render_template('register.html', userReg= 'userReg' in session)
+
 
 # Обработка ошибки 403
 @main.errorhandler(403)
