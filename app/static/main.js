6508295105ecd6767ab4a4980f98d1acb9e567c0
KrasnoeBeloe2211 <kirillburlaka.2006@gmail.com>
2024-10-25 21:20:56 +0300

    REGISTERJS2

--- app/static/main.js
+++ app/static/main.js
@@ -1,7 +1,14 @@
 document.getElementById('reg_form').addEventListener('submit', GetInput);
 
+valid_el = document.createElement('p');
+
     function GetInput(event) {
         event.PreventDefault();
+        el.appendChild(valid_el);
         var el = document.getElementById('reg_form')
+        if (el.log.value === "") {
+            valid_el.innerHTML=""
+
+        }
         
     };
