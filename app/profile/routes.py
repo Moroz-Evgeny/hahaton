9d2aae6f907f4ad65b711f10f0afa219b05c39bc
Moroz-Evgeny <ya.moroz2020@gmail.com>
2024-10-26 23:08:05 +0300

    profile

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -1,8 +1,13 @@
-import requests
 from flask import Blueprint, render_template, session, flash, redirect, url_for, request
-
+from app import db
+from app.models import User
 
 prof = Blueprint('prof', __name__)
 @prof.route('/profile')
 def profile():
-  return render_template('profile.html')
+  if 'userLogged' in session:
+    userLogin = User.query.filter_by(email=session['userEmail']).first().login
+    return render_template('profile.html', userLogin=userLogin, userEmail=session['userEmail'])
+  
+  flash('Войдите или зарегистрируйтесь', 'warning')
+  return redirect(url_for('main.register'))
