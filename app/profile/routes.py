341630e0eb70c3a2e57ff6311a68af98a0401867
Moroz-Evgeny <ya.moroz2020@gmail.com>
2024-10-26 23:18:38 +0300

    iduser prof

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -7,7 +7,7 @@ prof = Blueprint('prof', __name__)
 def profile():
   if 'userLogged' in session:
     userLogin = User.query.filter_by(email=session['userEmail']).first().login
-    return render_template('profile.html', userLogin=userLogin, userEmail=session['userEmail'])
+    return render_template('profile.html', userLogin=userLogin, userEmail=session['userEmail'], userId=session['userId'])
   
   flash('Войдите или зарегистрируйтесь', 'warning')
   return redirect(url_for('main.register'))
