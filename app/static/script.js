cc1e6204ee9a6991ffd306d8fc6ec0383ca5b73c
KrasnoeBeloe2211 <kirillburlaka.2006@gmail.com>
2024-10-26 20:15:56 +0300

    jsPOLU4ILOS

--- app/static/script.js
+++ app/static/script.js
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	var carbon_ratio = JSON.parse(ratioStr) // Преобразуем строку в массив
 
 	var chartTooltip = document.getElementById('toolTip').getContext('2d')
-
+	var chlen = [1,4,15,1,512]
 	var toolTip = new Chart(chartTooltip, {
 		type: 'line',
 		data: {
 			labels: date,
 			datasets: [
 				{
-					label: 'online tutorial subjects',
+					label: 'Углеродный след',
 					data: carbon_ratio,
 					backgroundColor: ['green'],
 					borderColor: ['black'],
@@ -40,8 +40,40 @@ document.addEventListener('DOMContentLoaded', () => {
 				},
 			},
 		},
+		
 	})
-
+	var el= document.getElementById('carbon_id')
+	el.innerHTML = `<p>${carbon_ratio[carbon_ratio.length - 1]}</p>`
 	console.log('date:', date)
 	console.log('carbon_ratio:', carbon_ratio)
 })
+
+
+function MouseOver() {
+	document.getElementById("menu_id").style.width= "4%";
+	document.getElementById("chart").style.width="50px";
+	document.getElementById("profile").style.width="50px";
+	document.getElementById("calculator").style.width="50px";
+	document.getElementById("notification").style.width="50px";
+	document.getElementById("settings").style.width="50px";
+	document.getElementById("arrow").style.rotate="90deg";
+	document.getElementById("arrow").style.width="30px";
+
+	
+}
+
+function MouseLeave() {
+	document.getElementById("menu_id").style.width= "1%";
+	document.getElementById("chart").style.width="0px";
+	document.getElementById("profile").style.width="0px";
+	document.getElementById("calculator").style.width="0px";
+	document.getElementById("notification").style.width="0px";
+	document.getElementById("settings").style.width="0px";
+	document.getElementById("arrow").style.rotate="0deg";
+	document.getElementById("arrow").style.width="20px";
+
+	
+}
+
+
+
